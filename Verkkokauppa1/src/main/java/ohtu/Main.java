d2c43abb7d161974e99136262804e03b3f7ae7ae
Anu Salminen <aesalmin@gmail.com>
2016-04-03 17:29:18 +0300

    tehtävä 10

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -1,23 +1,18 @@
 package ohtu;
 
 import ohtu.verkkokauppa.Kauppa;
-import ohtu.verkkokauppa.Kauppavarasto;
 import ohtu.verkkokauppa.Kirjanpito;
 import ohtu.verkkokauppa.KirjanpitoInterface;
-import ohtu.verkkokauppa.Pankki;
-import ohtu.verkkokauppa.PankkiInterface;
-import ohtu.verkkokauppa.Varasto;
-import ohtu.verkkokauppa.Viitegeneraattori;
-import ohtu.verkkokauppa.ViitegeneraattoriInterface;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        KirjanpitoInterface kirjanpito = new Kirjanpito();
-        Varasto varasto = new Kauppavarasto(kirjanpito);
-        PankkiInterface pankki = new Pankki(kirjanpito);
-        ViitegeneraattoriInterface viitegen = new Viitegeneraattori();
-        Kauppa kauppa = new Kauppa(varasto,pankki, viitegen );
+      
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+      
+        Kauppa kauppa = ctx.getBean(Kauppa.class);
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
@@ -36,6 +31,8 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
+        KirjanpitoInterface kirjanpito = ctx.getBean(Kirjanpito.class);
+        
         for (String tapahtuma :  kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
