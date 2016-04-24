a24199216827b020447923cc58f84f9d133512ab
Anu Salminen <aesalmin@gmail.com>
2016-04-24 23:03:49 +0300

    tennistehtävä

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -6,75 +6,54 @@ public class TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
-
+    private final String[] SCORE_NAMES = {"Love", "Fifteen", "Thirty", "Forty"};
+    private final int SCORE_MAX = 4; 
+    
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals(player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
     }
 
     public String getScore() {
+        if (m_score1==m_score2) {
+            return this.getEvenScore();
+        }
+        else if (m_score1>=SCORE_MAX || m_score2>=SCORE_MAX) {
+            return this.getScoreAtMax();
+        }
+        return this.getScoreUnderMax();
+    }
+    
+    private String getScoreAtMax(){
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) return "Advantage player1";
+        else if (minusResult ==-1)return "Advantage player2";
+        else if (minusResult>=2) return "Win for player1";
+        else return "Win for player2"; 
+    }
+    
+    private String getEvenScore(){
+        if(m_score1 >= SCORE_MAX){
+            return "Deuce";
+        }
+        return this.SCORE_NAMES[m_score1] + "-All";
+    }
+    
+    private String getScoreUnderMax(){
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+        for (int i=1; i<3; i++) {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            score += this.SCORE_NAMES[tempScore];
+        } 
         return score;
     }
 }
